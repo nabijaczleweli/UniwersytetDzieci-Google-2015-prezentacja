d5df1aca74794baf4e894afdb0fbfea057e61663
nabijaczleweli <nabijaczleweli@gmail.com>
2015-05-21 22:30:44 +0200

    Improve input demonstration

--- README.md
+++ README.md
@@ -3,4 +3,4 @@ UniwersytetDzieci-Google-2015-prezentacja
 
 Prezentacja na warsztaty z programowania aplikacji mobilnych w roku 2015, wykonanych przez [@nabijaczleweli](https://github.com/nabijaczleweli), Maję Miłek i Łucję Hannę Białą
 
-Note, that I will not count this repo towards commits for my streak -- [@nabijaczleweli](https://github.com/nabijaczleweli)
+Note, that I will not count this repo towards commits for my streak (except for JS-only commits) -- [@nabijaczleweli](https://github.com/nabijaczleweli)
--- prezentacja/przykłady/input/input.js
+++ prezentacja/przykłady/input/input.js
@@ -1,3 +1,6 @@
+// NOTE: This script requires ECMAScript 6 support (uses `const`)
+
+
 var inputs;
 
 const typeToCallback = {
@@ -12,6 +15,22 @@ const typeToCallback = {
 	'color': {
 		'eventName': 'change',
 		'callback': checkboxCallback
+	},
+	'email': {
+		'eventName': 'change',
+		'callback': emailCallback
+	},
+	'file': {
+		'eventName': 'change',
+		'callback': fileCallback
+	},
+	'password': {
+		'eventName': 'change',
+		'callback': passwordCallback
+	},
+	'radio': {
+		'eventName': 'change',
+		'callback': radioCallback
 	}
 };
 
@@ -22,6 +41,7 @@ window.onload = function() {
 	for(var i = 0; i < inputs.length; ++i) {
 		const input = inputs[i];
 		const type = input.getAttribute('type');
+
 		const nameWithCallback = typeToCallback[type];
 
 		input.nextElementSibling.textContent = type[0].toUpperCase() + type.substr(1);
@@ -30,7 +50,31 @@ window.onload = function() {
 
 		if('autocomplete' in input)
 			input.autocomplete = true;
+
+		const typeSupportedByBrowser = inputTypeSupported(type);
+		const typeSupportedByDemonstation = type in typeToCallback;
+		if(!typeSupportedByBrowser || !typeSupportedByDemonstation) {
+			const descriptionNode = document.createElement('SPAN')
+			const textNode = document.createTextNode((typeSupportedByBrowser ? '' : '!NOT SUPPORTED IN THIS BROWSER!') + (typeSupportedByDemonstation ? '' : ' !NOT SUPPORTED BY THE DEMONSTRATION!'));
+			descriptionNode.appendChild(textNode);
+			descriptionNode.className = 'unsupported';
+
+			input.parentNode.insertBefore(descriptionNode, input.nextElementSibling);
+		}
 	};
+}
+
+
+// Based on http://stackoverflow.com/a/21840624/2851815
+function inputTypeSupported(type) {
+	try {
+		var elem = document.createElement('input');
+		elem.type = type;
+		if(elem.type === type)
+			return true;
+	} catch(e) {}
+
+	return false;
 }
 
 
@@ -66,11 +110,87 @@ function checkboxCallback(e) {
 	if(descriptionNode.className !== '') {
 		const after = descriptionNode;
 
-		descriptionNode = document.createElement('SPAN')
+		descriptionNode = document.createElement('SPAN');
 		descriptionNode.appendChild(document.createTextNode(''));
 
 		input.parentNode.insertBefore(descriptionNode, after);
 	}
 
 	descriptionNode.textContent = input.checked;
+}
+
+function emailCallback(e) {
+	const input = e.originalTarget;
+	var anchorNode = input.nextElementSibling;
+
+	// Dynamically append description node
+	if(anchorNode.className !== '') {
+		const after = anchorNode;
+
+		anchorNode = document.createElement('A');
+		anchorNode.appendChild(document.createTextNode(''));
+
+		input.parentNode.insertBefore(anchorNode, after);
+	}
+
+	anchorNode.textContent = input.value;
+	anchorNode.href = 'mailto:' + input.value;
+}
+
+function fileCallback(e) {
+	const input = e.originalTarget;
+	var anchorNode = input.nextElementSibling;
+
+	// Dynamically append description node
+	if(anchorNode.className !== '') {
+		const after = anchorNode;
+
+		anchorNode = document.createElement('A');
+		anchorNode.appendChild(document.createTextNode(''));
+
+		input.parentNode.insertBefore(anchorNode, after);
+	}
+
+	anchorNode.textContent = 'Download/open ' + input.files[0].name;
+	anchorNode.href = URL.createObjectURL(input.files[0]);
+}
+
+function passwordCallback(e) {
+	const input = e.originalTarget;
+	var descriptionNode = input.nextElementSibling;
+
+	// Dynamically append description node
+	if(descriptionNode.className !== '') {
+		const after = descriptionNode;
+
+		descriptionNode = document.createElement('SPAN');
+		descriptionNode.appendChild(document.createTextNode(''));
+
+		input.parentNode.insertBefore(descriptionNode, after);
+	}
+
+	descriptionNode.textContent = 'Psst! Your password is "' + input.value + '"';
+}
+
+function radioCallback(e) {
+	const input = e.originalTarget;
+	var descriptionNode = input.nextElementSibling;
+
+	// Dynamically append description node
+	if(descriptionNode.className !== '') {
+		const after = descriptionNode;
+
+		descriptionNode = document.createElement('SPAN');
+		descriptionNode.appendChild(document.createTextNode(''));
+
+		input.parentNode.insertBefore(descriptionNode, after);
+	}
+
+	if(input.checked) {
+		descriptionNode.textContent = 'Noooooo! Why did you check it!';
+		setTimeout(function() {
+			descriptionNode.textContent = 'Crisis averted.';
+			input.checked = false;
+		}, 5000);
+	}
 }
